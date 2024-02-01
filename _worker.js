347553d50e30713a15b04d0a4b571acbe39da53d
diantakyid <62738567+diantakyid@users.noreply.github.com>
2024-02-02 02:07:11 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'aff60020-2830-4539-87ed-e692255d7751';
 
-const proxyIPs = ['163.181.100.110'];
+const proxyIPs = ['163.181.19.169'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
